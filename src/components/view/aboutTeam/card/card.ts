c0fa178eeab12a9b8951e005daee7ff6d7b8ada6
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-08-17 17:22:39 +0300

    add hover img person

--- src/components/view/aboutTeam/card/card.ts
+++ src/components/view/aboutTeam/card/card.ts
@@ -2,8 +2,18 @@ import { createImg, createText } from '../../../../utils/HTMLBuilder';
 import { IComponentUl } from '../../InterfaseComponent';
 
 const anchorGitHub = ['https://github.com/nikimix', 'https://github.com/SmSka2021', 'https://github.com/ekambalov'];
-const name = ['Никита', 'Светлана', 'Егор'];
+const name = ['Никита', 'Светлана', 'Ягор'];
 const work = ['frontend developer', 'frontend developer', 'frontend developer'];
+const avatar = [
+    '../../../../assets/img/avatar35.png',
+    '../../../../assets/img/avatar21.png',
+    '../../../../assets/img/avatar34.png',
+];
+const avatarHover = [
+    '../../../../assets/img/avatar3.png',
+    '../../../../assets/img/avatar2.png',
+    '../../../../assets/img/avatar01.png',
+];
 
 class CardTeam implements IComponentUl {
     start(): HTMLUListElement {
@@ -14,7 +24,13 @@ class CardTeam implements IComponentUl {
             const cardLI = document.createElement('li') as HTMLLIElement;
             cardLI.classList.add(`cards_card${i}`);
 
-            const imgPerson = createImg('../../../../assets/img/unnamed.png', 'foto autor', ['cards__card_imgTeam']);
+            const imgPerson = createImg(avatar[i], 'foto autor', ['cards__card_imgTeam']);
+            imgPerson.addEventListener('mouseover', () => {
+                imgPerson.src = avatarHover[i];
+            });
+            imgPerson.addEventListener('mouseleave', () => {
+                imgPerson.src = avatar[i];
+            });
             const namePerson = createText(`${name[i]}`, 'h3', ['cards__card_name']);
             const workPerson = createText(`${work[i]}`, 'h4', ['cards__card_work']);
 
