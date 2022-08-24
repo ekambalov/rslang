6b787fd6d47abf2093337b923eaa736462f03e28
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-08-24 14:04:21 +0300

    feat: add feth autorise

--- src/model/state.ts
+++ src/model/state.ts
@@ -17,12 +17,3 @@ const State: IState = {
 };
 
 export default State;
-/*
-{
-  message: 'Authenticated',
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzM…DEwfQ.xyv_ZiSWrMt78-2bYb4Mv23tnVovVaE1s5-P9AuNbBQ',
-  refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzM…YxMH0.fNH5LR_0Pox6QTlsngmt-MfrOoebH7eNgg8KaxUJtd0',
-  userId: '63054b928d651600159f91d4',
-  name: 'fvgyynu'
-    }
-    */
